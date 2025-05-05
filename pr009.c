faa808095d4db2964d55e5f0c835a5a006b67b27
Ekaterina <filkaterina69@gmail.com>
2025-05-05 16:29:32 +0700

    Программа записывающая и читающая информацию из пайпа

---
+++ pr009.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main() {
+        int fd[2];
+        size_t size;
+        char string[] = "IFMITO. Gr.3.008.2.24\n", resstring[22];
+ 	//Создание пайп
+	if(pipe(fd)<0) {
+		printf("неудалось создать пайп\n");
+		exit(-1);
+			}
+	size = write(fd[1], string, 22);
+	if(size !=22){
+		printf("не удалось записать всю строку в пайп \n");
+		exit(-1);
+	}
+	if(close(fd[1]) < 0) {
+		printf("если не получилось закрыть входной поток\n");
+		exit(-1);
+			}
+	size = read(fd[0], resstring, 22);
+	if(size != 22){
+                printf("не удалось прочитать всю строку из пайпа \n");
+		exit(-1);
+	}
+	printf("прочитаная из пайпа строка: %s", resstring);
+	if(close(fd[0])<0) {
+		printf("не удалось закрыть входной поток\n");
+		exit(-1);
+	}
+	return 0;
+}
